703cf89572a8bf4fda2a0541fa6715831549fd50
AstroB20 <bhisman20@gmail.com>
2025-02-18 23:26:41 +0530

    hmm

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.routes import auction_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tiger@localhost/auction_system'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Tiger@localhost/auction_system'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
